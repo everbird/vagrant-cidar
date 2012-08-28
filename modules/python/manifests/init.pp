0a3d5cf5b22230d20a0b52d44dcb1a24835dcdc8
Stephen Zhuang <everbird@douban.com>
2012-08-28 13:45:17 +0800

    Hack to change /etc/portage/package.keywords/puppet 600=>644

--- modules/python/manifests/init.pp
+++ modules/python/manifests/init.pp
@@ -11,6 +11,11 @@ class python {
     target      => 'puppet',
   }
 
+  # puppet-gentoo would create puppet file with mode 600, which cause eix faild when provision
+  file { '/etc/portage/package.keywords/puppet':
+    mode => 644,
+  }
+
   package { 'dev-python/virtualenvwrapper':
     ensure => latest
   }
