f6863456c04d70ad65d846ab1ee879570978142d
Stephen Zhuang <stephen.zhuang@gmail.com>
2013-10-28 18:54:47 +0800

    Add some tools for stat

--- modules/infrastructure/manifests/init.pp
+++ modules/infrastructure/manifests/init.pp
@@ -7,6 +7,10 @@ class infrastructure() {
   package {[
     'app-misc/tmux',
     'sys-process/lsof',
+    'dev-util/strace',
+    'net-analyzer/ifstat',
+    'sys-process/htop',
+    'app-admin/sysstat',
   ]:
     ensure => latest
   }
